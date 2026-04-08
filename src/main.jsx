44cf5966c0dd18178680036a75a8b50c58377622
Abdulsmh <68156118+Abdulsmh@users.noreply.github.com>
2026-04-09 11:55:30 +1300

    Fix deployment with relative paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom"; // CHANGE THIS
-import { Toaster } from "react-hot-toast"; // ADD THIS
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
-      {" "}
       <App />
-      <Toaster />
     </HashRouter>
   </StrictMode>,
 );
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/ama_farm_connect_v2/",
+  base: "./",
   server: {
     port: 3000,
     open: true,
