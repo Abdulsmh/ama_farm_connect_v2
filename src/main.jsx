6004783f5bc15891da3baff2ba81e6aa909187bb
Abdulsmh <68156118+Abdulsmh@users.noreply.github.com>
2026-04-09 11:49:54 +1300

    Fix deployment: HashRouter and Toaster

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom"; // CHANGE THIS
+import { Toaster } from "react-hot-toast"; // ADD THIS
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
+      {" "}
       <App />
-    </BrowserRouter>
+      <Toaster />
+    </HashRouter>
   </StrictMode>,
-)
+);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-})
+  plugins: [tailwindcss(), react()],
+  base: "/ama_farm_connect_v2/",
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
